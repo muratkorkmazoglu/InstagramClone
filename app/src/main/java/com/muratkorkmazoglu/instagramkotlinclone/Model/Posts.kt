986021c2c1976fa7a4b399caa57af1773cfb82b3
muratkorkmazoglu <muratkorkmazoglu@gmail.com>
2018-11-28 14:43:22 +0300

    home posts start

--- app/src/main/java/com/muratkorkmazoglu/instagramkotlinclone/Model/Posts.kt
+++ app/src/main/java/com/muratkorkmazoglu/instagramkotlinclone/Model/Posts.kt
@@ -3,12 +3,12 @@ package com.muratkorkmazoglu.instagramkotlinclone.Model
 class Posts {
     var user_id: String? = null
     var post_id: String? = null
-    var yuklenme_tarih: String? = null
+    var yuklenme_tarih: Long? = null
     var aciklama: String? = null
     var file_url: String? = null
 
     constructor()
-    constructor(user_id: String?, post_id: String?, yuklenme_tarih: String?, aciklama: String?, file_url: String?) {
+    constructor(user_id: String?, post_id: String?, yuklenme_tarih: Long?, aciklama: String?, file_url: String?) {
         this.user_id = user_id
         this.post_id = post_id
         this.yuklenme_tarih = yuklenme_tarih
---
+++ app/src/main/java/com/muratkorkmazoglu/instagramkotlinclone/Model/UserPosts.kt
@@ -0,0 +1,10 @@
+package com.muratkorkmazoglu.instagramkotlinclone.Model
+
+class UserPosts {
+    var userId: String? = null
+    var userName: String? = null
+    var userPostUrl: String? = null
+    var postId: String? = null
+    var postAciklama: String? = null
+    var postURL: String? = null
+}
